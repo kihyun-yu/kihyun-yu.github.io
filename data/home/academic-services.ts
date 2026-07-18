d02ebe6f31029d982df4b17bd8f729177028b101
kihyun-yu <khyu99@kaist.ac.kr>
2026-07-18 22:22:19 +0900

    .

--- data/home/academic-services.ts
+++ data/home/academic-services.ts
@@ -1,4 +1,4 @@
 export const academicServicesList = [
-  'Conference Reviewer: ICML 2026 ([silver reviewer](https://icml.cc/Conferences/2026/ProgramCommittee)), NeurIPS 2026, IEEE CDC 2026',
+  'Conference Reviewer: ICML 2026 ([silver reviewer](https://icml.cc/Conferences/2026/ProgramCommittee)), NeurIPS 2026, IEEE CDC 2026, AAAI 2027',
   'Journal Reviewer: TMLR',
 ];
